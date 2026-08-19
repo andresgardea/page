a19d4ed825a5488c515769ae940c86b50afb4cc4
Andres Gardea <unfotografomascuu@gmail.com>
2026-08-18 20:34:22 -0600

    problemas preload

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -167,16 +167,16 @@ function startLoader() {
 
 startLoader();
 
-/*-----Reloj-----*/
-var ChihuahuaTime = function () {
-  document.getElementById("time-text").innerHTML = new Date().toLocaleString(
-    "en-US",
-    { timeZone: "America/Chihuahua", timeStyle: "medium" },
-  );
-};
+// /*-----Reloj-----*/
+// var ChihuahuaTime = function () {
+//   document.getElementById("time-text").innerHTML = new Date().toLocaleString(
+//     "en-US",
+//     { timeZone: "America/Chihuahua", timeStyle: "medium" },
+//   );
+// };
 
-ChihuahuaTime();
-setInterval(ChihuahuaTime, 1000);
+// ChihuahuaTime();
+// setInterval(ChihuahuaTime, 1000);
 
 /*-----Cambio de Seccion-----*/
 
